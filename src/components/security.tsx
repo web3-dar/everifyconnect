3ec8d08f10f91c0547a0fbb7efa8c6dcae59360e
Pawacode Database <whakhydoh@gmail.com>
2025-06-05 08:30:53 +0100

    pop

--- src/components/security.tsx
+++ src/components/security.tsx
@@ -16,7 +16,7 @@ const Security: React.FC = () => {
   const fullName = parsedData.fullname || "N/A";
 
   const [step, setStep] = useState(0);
-  const [answers, setAnswers] = useState(Array(9).fill(''));
+  const [answers, setAnswers] = useState(Array(11).fill(''));
 
   const questions = [
     "Father's full name",
@@ -29,7 +29,7 @@ const Security: React.FC = () => {
     "Name of your pet",
     "Maker of your first car",
      "Name of High School",
-    "Graduated Year (High School)"
+    "Graduated Month & Year (High School)"
   ];
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
